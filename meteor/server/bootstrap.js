7fd1d3d58c5a3912a738de0e35c2fe9e842d3db7
Villamizar Irwin <erenss.sk8@gmail.com>
2012-11-03 14:55:19 +0100

    add some bands in bootstrap

--- meteor/server/bootstrap.js
+++ meteor/server/bootstrap.js
@@ -5,4 +5,26 @@
  */
 Meteor.startup(function() {
 
+	/**
+	* Quelques données à afficher
+	* si BDD vide. (prévisionnel).
+	**/
+ 	// if (Lists.find().count() === 0) {
+	    var data = [
+			{name: "Bands in top 10",
+				contents: [
+					["GMH", "France", "Melodic Punk Metal"],
+					["Arch Enemy", "Swede", "Deth Metal Melodic"],
+					["SoilWork", "Swede", "Deth Metal Melodic"],
+					["Propagandhi", "Canada", "Punk"],
+					["NoFx", "US", "Punk Rock"],
+					["Disarmonia Mundi", "Italy", "Deth Metal Melodic"],
+					["Bad Relegion", "US", "Punk Rock"],
+					["Iron Maiden", "England", "Heavy Metal"],
+					["L'esprit du clan", "France", "Hardcore Metal"],
+					["Ten Foot Pole", "US", "Punk Rock"]
+				]
+			}
+	    ];
+	// }
 });
